cfe9e4c0ff777e939edb3d4e68a145b84f69d20e
atomize <63r71@tuta.io>
2019-08-23 22:47:23 +0200

    update example test

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
     <div className="App">   
-      <GithubIssues  user="angular" repo="angular" />
+      <GithubIssues  user="facebook" repo="react" />
     </div>
   </>
   );
