80d865c91767b5f5dbf852d1dddd56e9a6c1935f
atomize <63r71@tuta.io>
2019-08-23 23:12:26 +0200

    add docs for ghPages

---
+++ docs/precache-manifest.26d94d47fcb40443b6dad82f52ed7346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035e274c387ecb6b231910c17cef5c5b",
+    "url": "/github-issues-react-example/docs/index.html"
+  },
+  {
+    "revision": "fdc95b467fc29db1a04c",
+    "url": "/github-issues-react-example/docs/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "c2c848953ac03f15287d",
+    "url": "/github-issues-react-example/docs/static/css/main.c337ba1b.chunk.css"
+  },
+  {
+    "revision": "fdc95b467fc29db1a04c",
+    "url": "/github-issues-react-example/docs/static/js/2.a2a2928c.chunk.js"
+  },
+  {
+    "revision": "c2c848953ac03f15287d",
+    "url": "/github-issues-react-example/docs/static/js/main.2e96f077.chunk.js"
+  },
+  {
+    "revision": "1d6a18857da8b88f4ae4",
+    "url": "/github-issues-react-example/docs/static/js/runtime~main.1f789fd5.js"
+  }
+]);
