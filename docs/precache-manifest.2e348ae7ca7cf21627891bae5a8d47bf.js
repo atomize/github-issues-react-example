6b837f94e046c482b26c6f21d3d5c76497e4a2d4
Berti <jonathan.berti@gmail.com>
2019-08-26 02:26:52 -0500

    fix props, update build/readme

---
+++ docs/precache-manifest.2e348ae7ca7cf21627891bae5a8d47bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81baf75665b01099f7279cdec222eb29",
+    "url": "/github-issues-react-example/index.html"
+  },
+  {
+    "revision": "61e25bb2850d84f1c997",
+    "url": "/github-issues-react-example/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "97c6a5695967a93e33b2",
+    "url": "/github-issues-react-example/static/css/main.4c17a9bd.chunk.css"
+  },
+  {
+    "revision": "61e25bb2850d84f1c997",
+    "url": "/github-issues-react-example/static/js/2.43db596b.chunk.js"
+  },
+  {
+    "revision": "97c6a5695967a93e33b2",
+    "url": "/github-issues-react-example/static/js/main.d1b87034.chunk.js"
+  },
+  {
+    "revision": "7f4576475cc2ffcfe944",
+    "url": "/github-issues-react-example/static/js/runtime~main.03d5f8c1.js"
+  }
+]);
