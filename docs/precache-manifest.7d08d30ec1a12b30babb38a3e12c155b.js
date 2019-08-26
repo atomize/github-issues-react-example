4823a0ae905238cfd6fac9b1f45ca7440c4ad06e
Berti <jonathan.berti@gmail.com>
2019-08-26 02:33:20 -0500

    rebuild docs

---
+++ docs/precache-manifest.7d08d30ec1a12b30babb38a3e12c155b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66256de9b37e6ee15471363868489a28",
+    "url": "/github-issues-react-example/index.html"
+  },
+  {
+    "revision": "61e25bb2850d84f1c997",
+    "url": "/github-issues-react-example/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "f3b7282c9b3cdf5085e4",
+    "url": "/github-issues-react-example/static/css/main.4c17a9bd.chunk.css"
+  },
+  {
+    "revision": "61e25bb2850d84f1c997",
+    "url": "/github-issues-react-example/static/js/2.43db596b.chunk.js"
+  },
+  {
+    "revision": "f3b7282c9b3cdf5085e4",
+    "url": "/github-issues-react-example/static/js/main.dddc464e.chunk.js"
+  },
+  {
+    "revision": "7f4576475cc2ffcfe944",
+    "url": "/github-issues-react-example/static/js/runtime~main.03d5f8c1.js"
+  }
+]);
